8ce13c086569489d489d3166b16b087bae2daf5f
mhy-666 <57670156+mhy-666@users.noreply.github.com>
2024-04-13 06:26:35 +0000

    update data.py

--- src/data.py
+++ src/data.py
@@ -32,18 +32,18 @@ class SampleGenerator(object):
     def __init__(self, ratings):
         """
         args:
-            ratings: pd.DataFrame, which contains 4 columns = ['user_id', 'anime_id', 'rating', 'watching_status', 'watched_episodes']
+            ratings: pd.DataFrame, which contains 4 columns = ['userId', 'itemId', 'rating', 'watching_status', 'watched_episodes']
         """
-        assert 'user_id' in ratings.columns
-        assert 'anime_id' in ratings.columns
+        assert 'userId' in ratings.columns
+        assert 'itemId' in ratings.columns
         assert 'rating' in ratings.columns
 
         self.ratings = ratings
         # explicit feedback using _normalize and implicit using _binarize
         # self.preprocess_ratings = self._normalize(ratings)
         self.preprocess_ratings = self._binarize(ratings)
-        self.user_pool = set(self.ratings['user_id'].unique())
-        self.item_pool = set(self.ratings['anime_id'].unique())
+        self.user_pool = set(self.ratings['userId'].unique())
+        self.item_pool = set(self.ratings['itemId'].unique())
         # create negative item samples for NCF learning
         self.negatives = self._sample_negative(ratings)
         self.train_ratings, self.test_ratings = self._split_loo(self.preprocess_ratings)
@@ -63,31 +63,31 @@ class SampleGenerator(object):
 
     def _split_loo(self, ratings):
         """leave one out train/test split """
-        ratings['rank_latest'] = ratings.groupby(['user_id'])['watched_episodes'].rank(method='first', ascending=False)
+        ratings['rank_latest'] = ratings.groupby(['userId'])['watched_episodes'].rank(method='first', ascending=False)
         test = ratings[ratings['rank_latest'] == 1]
         train = ratings[ratings['rank_latest'] > 1]
-        assert train['user_id'].nunique() == test['user_id'].nunique()
-        return train[['user_id', 'anime_id', 'rating']], test[['user_id', 'anime_id', 'rating']]
+        assert train['userId'].nunique() == test['userId'].nunique()
+        return train[['userId', 'itemId', 'rating']], test[['userId', 'itemId', 'rating']]
 
     def _sample_negative(self, ratings):
         """return all negative items & 100 sampled negative items"""
-        interact_status = ratings.groupby('user_id')['anime_id'].apply(set).reset_index().rename(
-            columns={'anime_id': 'interacted_items'})
+        interact_status = ratings.groupby('userId')['itemId'].apply(set).reset_index().rename(
+            columns={'itemId': 'interacted_items'})
         interact_status['negative_items'] = interact_status['interacted_items'].apply(lambda x: self.item_pool - x)
         interact_status['negative_samples'] = interact_status['negative_items'].apply(lambda x: random.sample(x, 99))
-        return interact_status[['user_id', 'negative_items', 'negative_samples']]
+        return interact_status[['userId', 'negative_items', 'negative_samples']]
 
     def instance_a_train_loader(self, num_negatives, batch_size):
         """instance train loader for one training epoch"""
         users, items, ratings = [], [], []
-        train_ratings = pd.merge(self.train_ratings, self.negatives[['user_id', 'negative_items']], on='user_id')
+        train_ratings = pd.merge(self.train_ratings, self.negatives[['userId', 'negative_items']], on='userId')
         train_ratings['negatives'] = train_ratings['negative_items'].apply(lambda x: random.sample(x, num_negatives))
         for row in train_ratings.itertuples():
-            users.append(int(row.user_id))
-            items.append(int(row.anime_id))
+            users.append(int(row.userId))
+            items.append(int(row.itemId))
             ratings.append(float(row.rating))
             for i in range(num_negatives):
-                users.append(int(row.user_id))
+                users.append(int(row.userId))
                 items.append(int(row.negatives[i]))
                 ratings.append(float(0))  # negative samples get 0 rating
         dataset = UserItemRatingDataset(user_tensor=torch.LongTensor(users),
@@ -98,13 +98,13 @@ class SampleGenerator(object):
     @property
     def evaluate_data(self):
         """create evaluate data"""
-        test_ratings = pd.merge(self.test_ratings, self.negatives[['user_id', 'negative_samples']], on='user_id')
+        test_ratings = pd.merge(self.test_ratings, self.negatives[['userId', 'negative_samples']], on='userId')
         test_users, test_items, negative_users, negative_items = [], [], [], []
         for row in test_ratings.itertuples():
-            test_users.append(int(row.user_id))
-            test_items.append(int(row.anime_id))
+            test_users.append(int(row.userId))
+            test_items.append(int(row.itemId))
             for i in range(len(row.negative_samples)):
-                negative_users.append(int(row.user_id))
+                negative_users.append(int(row.userId))
                 negative_items.append(int(row.negative_samples[i]))
         return [torch.LongTensor(test_users), torch.LongTensor(test_items), torch.LongTensor(negative_users),
                 torch.LongTensor(negative_items)]
